3ca6bb2ae5b00e05dee7b273aeab00290271a934
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-10-13 22:56:03 -0300

    implementando basic auth

---
+++ src/main/java/br/com/joelton/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,12 @@
+package br.com.joelton.todolist.filter;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class FilterTaskAuth implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // Executar alguma ação
+    }
+}
